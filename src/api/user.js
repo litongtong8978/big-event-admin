9e0177aa59521436067235630188a0dca41f7d06
litongtong <18831013829@163.com>
2025-03-11 11:20:27 +0800

    个人中心

--- src/api/user.js
+++ src/api/user.js
@@ -10,3 +10,12 @@ export const userLoginService = ({ username, password }) => {
 export const userGetInfoService = () => {
   return request.get('/my/userinfo')
 }
+export const userUpdateInfoService = ({ id, nickname, email }) => {
+  return request.put('/my/userinfo', { id, nickname, email })
+}
+export const userUpdateAvatarService = (avatar) =>
+  request.patch('/my/update/avatar', { avatar })
+
+// 更新用户密码
+export const userUpdatePasswordService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
